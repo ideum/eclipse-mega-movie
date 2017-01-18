373cd8235582aed5b03a8ace6fc2d54d9c806de9
nmbrax <braxton@ideum.com>
2017-01-18 14:37:37 -0700

    initial commit
    created basic 'hello world' project

---
+++ MegaMovie/app/src/main/java/ideum/com/megamovie/BasicCameraActivity.java
@@ -0,0 +1,13 @@
+package ideum.com.megamovie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BasicCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_camera);
+    }
+}
