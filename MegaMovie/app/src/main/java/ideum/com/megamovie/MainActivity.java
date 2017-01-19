963933bc874ff787906d8483fb287f097eecfd9c
nmbrax <braxton@ideum.com>
2017-01-18 17:08:38 -0700

    added map
    Added a google map view together with a tappable location marker

--- MegaMovie/app/src/main/java/ideum/com/megamovie/MainActivity.java
+++ MegaMovie/app/src/main/java/ideum/com/megamovie/MainActivity.java
@@ -3,11 +3,32 @@ package ideum.com.megamovie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location
+        LatLng washingtonDC = new LatLng(38.9072,-77.0369);
+        googleMap.addMarker(new MarkerOptions().position(washingtonDC).title("Marker in Washington"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(washingtonDC));
     }
 }
