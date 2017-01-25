f926e2818e1d99b83df125e9e12831a2af551604
nmbrax <braxton@ideum.com>
2017-01-25 13:27:26 -0700

    improved file saving procedure
    refactored code to update ImageSaver class and created new
    ImageSaverBuilder nested class. Added a queue for saving files
    asynchronously.

--- MegaMovie/app/src/main/java/ideum/com/megamovie/Java/CameraActivity.java
+++ MegaMovie/app/src/main/java/ideum/com/megamovie/Java/CameraActivity.java
@@ -10,11 +10,13 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
 import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.icu.text.SimpleDateFormat;
 import android.media.Image;
 import android.media.ImageReader;
+import android.os.AsyncTask;
 import android.os.CountDownTimer;
 import android.os.Environment;
 import android.os.Handler;
@@ -38,10 +40,14 @@ import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CameraActivity extends AppCompatActivity {
     private static boolean USES_TIMER = true;
-    private static int TIMER_LENGTH = 2000;
+    private static int TIMER_LENGTH = 1000;
     private static int TIMER_INTERVAL = 100;
     private static long SENSOR_EXPOSURE_TIME = 5*1000000;
     private static int SENSOR_SENSITIVITY = 1000;
@@ -62,18 +68,40 @@ public class CameraActivity extends AppCompatActivity {
 
     private CameraManager mCameraManager;
     private CameraDevice mCameraDevice;
+    private CameraCharacteristics mCharacteristics;
+    private final AtomicInteger mRequestCounter = new AtomicInteger();
     private CameraCaptureSession mCameraCaptureSession;
     private CameraCaptureSession.CaptureCallback mCaptureSessionCallback =
+
             new CameraCaptureSession.CaptureCallback() {
+
+                @Override
+                public void onCaptureStarted(CameraCaptureSession session, CaptureRequest request, long timestamp, long frameNumber) {
+                    super.onCaptureStarted(session, request, timestamp, frameNumber);
+
+                    String currentDataTime = generateTimeStamp();
+                    File jpegFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                            "JPEG_" + currentDataTime + ".jpg");
+                    ImageSaver.ImageSaverBuilder jpegBuilder;
+                    int requestId = (int) request.getTag();
+                    jpegBuilder = mJpegResultQueue.get(requestId);
+                    if (jpegBuilder != null) {
+                        jpegBuilder.setFile (jpegFile);
+                    }
+                }
 
                 @Override
                 public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
                     super.onCaptureCompleted(session, request, result);
-//                   captureStillImage();
+
+                    int requestId = (int) request.getTag();
+                    ImageSaver.ImageSaverBuilder jpegBuilder = mJpegResultQueue.get(requestId);
+                    if (jpegBuilder != null) {
+                        jpegBuilder.setResult(result);
+                    }
                 }
             };
 
-    private CaptureRequest.Builder mCaptureRequestBuilder;
     private CameraDevice.StateCallback mCameraDeviceStateCallback =
             new CameraDevice.StateCallback() {
                 @Override
@@ -100,49 +128,17 @@ public class CameraActivity extends AppCompatActivity {
 
     private HandlerThread mBackgroundThread;
     private Handler mBackgroundHandler;
-    private static File mImageFile;
-    private ImageReader mImageReader;
+    private final TreeMap<Integer, ImageSaver.ImageSaverBuilder> mJpegResultQueue = new TreeMap<>();
+
+    private ImageReader mJpegReader;
     private final ImageReader.OnImageAvailableListener mOnImageAvailableListener =
             new ImageReader.OnImageAvailableListener() {
                 @Override
                 public void onImageAvailable(ImageReader reader) {
-                        mBackgroundHandler.post(new ImageSaver(mImageReader.acquireNextImage()));
+                        dequeueAndSaveImage(mJpegResultQueue,mJpegReader);
 
                 }
             };
-    private static class ImageSaver implements Runnable {
-
-        private final Image mImage;
-
-        private ImageSaver(Image image) {
-            mImage = image;
-        }
-
-        @Override
-        public void run() {
-            ByteBuffer byteBuffer = mImage.getPlanes()[0].getBuffer();
-            byte[] bytes = new byte[byteBuffer.remaining()];
-            byteBuffer.get(bytes);
-
-            FileOutputStream fileOutputStream = null;
-
-            try {
-                fileOutputStream = new FileOutputStream(mImageFile);
-                fileOutputStream.write(bytes);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                mImage.close();
-                if(fileOutputStream != null) {
-                    try {
-                        fileOutputStream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-    }
 
     public void loadMapActivity(View view) {
 
@@ -192,26 +188,36 @@ public class CameraActivity extends AppCompatActivity {
     }
 
     public void takePhoto() {
-        try {
-            mImageFile = createImageFile();
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            mImageFile = createImageFile();
+//        } catch(IOException e) {
+//            e.printStackTrace();
+//        }
         captureStillImage();
     }
 
     private void captureStillImage() {
         try {
-            mCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_MANUAL);
-            mCaptureRequestBuilder.addTarget(mImageReader.getSurface());
+            final CaptureRequest.Builder captureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_MANUAL);
+            captureRequestBuilder.addTarget(mJpegReader.getSurface());
 
             int rotation = getWindowManager().getDefaultDisplay().getRotation();
-            mCaptureRequestBuilder.set(CaptureRequest.JPEG_ORIENTATION,getOrientation(rotation));
-            mCaptureRequestBuilder.set(CaptureRequest.SENSOR_SENSITIVITY,SENSOR_SENSITIVITY);
-            mCaptureRequestBuilder.set(CaptureRequest.SENSOR_EXPOSURE_TIME, SENSOR_EXPOSURE_TIME);
-            mCameraCaptureSession.capture(mCaptureRequestBuilder.build(),
+            captureRequestBuilder.set(CaptureRequest.JPEG_ORIENTATION,getOrientation(rotation));
+            captureRequestBuilder.set(CaptureRequest.SENSOR_SENSITIVITY,SENSOR_SENSITIVITY);
+            captureRequestBuilder.set(CaptureRequest.SENSOR_EXPOSURE_TIME, SENSOR_EXPOSURE_TIME);
+            captureRequestBuilder.setTag(mRequestCounter.getAndIncrement());
+
+            CaptureRequest request = captureRequestBuilder.build();
+
+            ImageSaver.ImageSaverBuilder jpegBuilder = new ImageSaver.ImageSaverBuilder().setCharacteristics(mCharacteristics);
+
+            mJpegResultQueue.put((int) request.getTag(),jpegBuilder);
+
+            mCameraCaptureSession.capture(request,
                     mCaptureSessionCallback,
                     mBackgroundHandler);
+
+
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
@@ -220,7 +226,7 @@ public class CameraActivity extends AppCompatActivity {
     private void createCameraSession() {
         try {
 
-            mCameraDevice.createCaptureSession(Arrays.asList(mImageReader.getSurface()),
+            mCameraDevice.createCaptureSession(Arrays.asList(mJpegReader.getSurface()),
                     new CameraCaptureSession.StateCallback() {
                         @Override
                         public void onConfigured(CameraCaptureSession session) {
@@ -268,8 +274,9 @@ public class CameraActivity extends AppCompatActivity {
                 if (cc.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT) {
                     continue;
                 }
+                mCharacteristics = cc;
                 StreamConfigurationMap map = cc.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
-                mImageSize = Collections.min(
+                mImageSize = Collections.max(
                         Arrays.asList(map.getOutputSizes(ImageFormat.JPEG)),
                         new Comparator<Size>() {
                             @Override
@@ -279,11 +286,11 @@ public class CameraActivity extends AppCompatActivity {
                             }
                         }
                 );
-                mImageReader = ImageReader.newInstance(mImageSize.getWidth(),
+                mJpegReader = ImageReader.newInstance(mImageSize.getWidth(),
                         mImageSize.getHeight(),
                         ImageFormat.JPEG,
                         /*max images */5);
-                mImageReader.setOnImageAvailableListener(mOnImageAvailableListener,mBackgroundHandler);
+                mJpegReader.setOnImageAvailableListener(mOnImageAvailableListener,mBackgroundHandler);
                 mCameraID = cameraID;
                 mSensorOrientation = cc.get(CameraCharacteristics.SENSOR_ORIENTATION);
             }
@@ -292,21 +299,21 @@ public class CameraActivity extends AppCompatActivity {
         }
     }
 
-    File createImageFile() throws IOException {
-        Calendar calendar = Calendar.getInstance();
-        String timeStamp = String.valueOf(calendar.getTimeInMillis());
-        String imageFileName = "IMAGE_" + timeStamp + "_";
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},
-                    0);
-        }
-        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-
-        File image = File.createTempFile(imageFileName,".jpg", storageDirectory);
-
-        return image;
-    }
+//    File createImageFile() throws IOException {
+//        Calendar calendar = Calendar.getInstance();
+//        String timeStamp = String.valueOf(calendar.getTimeInMillis());
+//        String imageFileName = "IMAGE_" + timeStamp + "_";
+//        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},
+//                    0);
+//        }
+//        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+//
+//        File image = File.createTempFile(imageFileName,".jpg", storageDirectory);
+//
+//        return image;
+//    }
 
     private void openBackgroundThread() {
         mBackgroundThread = new HandlerThread("Camera2 background thread");
@@ -324,4 +331,136 @@ public class CameraActivity extends AppCompatActivity {
         mBackgroundThread = null;
         mBackgroundHandler = null;
     }
+
+
+    private void dequeueAndSaveImage(TreeMap<Integer, ImageSaver.ImageSaverBuilder> pendingQueue,
+                                     ImageReader reader) {
+        Map.Entry<Integer, ImageSaver.ImageSaverBuilder> entry = pendingQueue.firstEntry();
+        ImageSaver.ImageSaverBuilder builder = entry.getValue();
+
+        Image image;
+        try {
+            image = reader.acquireNextImage();
+        } catch(IllegalStateException e) {
+            e.printStackTrace();
+            return;
+        }
+        builder.setReader(reader).setImage(image);
+        handleCompletionLocked(entry.getKey(),builder,pendingQueue);
+    }
+
+    private void handleCompletionLocked(int requestId, ImageSaver.ImageSaverBuilder builder,
+                                        TreeMap<Integer, ImageSaver.ImageSaverBuilder> queue) {
+        if (builder == null) return;
+        ImageSaver saver = builder.buildIfComplete();
+        if (saver != null) {
+            queue.remove(requestId);
+            AsyncTask.THREAD_POOL_EXECUTOR.execute(saver);
+        }
+    }
+
+    private static class ImageSaver implements Runnable {
+
+        private final Image mImage;
+        private final File mFile;
+        private final CaptureResult mCaptureResult;
+        private final CameraCharacteristics mCharacteristics;
+        private final ImageReader mReader;
+
+        private ImageSaver(Image image, File file, CaptureResult captureResult,
+                           CameraCharacteristics characteristics, ImageReader imageReader) {
+            mImage = image;
+            mFile = file;
+            mCaptureResult = captureResult;
+            mCharacteristics = characteristics;
+            mReader = imageReader;
+        }
+
+        @Override
+        public void run() {
+            ByteBuffer byteBuffer = mImage.getPlanes()[0].getBuffer();
+            byte[] bytes = new byte[byteBuffer.remaining()];
+            byteBuffer.get(bytes);
+
+            FileOutputStream fileOutputStream = null;
+
+            try {
+                fileOutputStream = new FileOutputStream(mFile);
+                fileOutputStream.write(bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                mImage.close();
+                if(fileOutputStream != null) {
+                    try {
+                        fileOutputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        public static class ImageSaverBuilder {
+            private Image mImage;
+            private File mFile;
+            private CaptureResult mCaptureResult;
+            private CameraCharacteristics mCharacteristics;
+            private ImageReader mReader;
+
+            public synchronized ImageSaverBuilder setReader(ImageReader reader) {
+                if (reader == null) throw new NullPointerException();
+                mReader = reader;
+                return this;
+            }
+
+            public synchronized ImageSaverBuilder setImage(final Image image) {
+                if (image == null) throw new NullPointerException();
+                mImage = image;
+                return this;
+            }
+
+            public synchronized ImageSaverBuilder setFile(final File file) {
+                if (file == null) throw new NullPointerException();
+                mFile = file;
+                return this;
+            }
+
+            public synchronized ImageSaverBuilder setResult(final CaptureResult result) {
+                if (result == null) throw new NullPointerException();
+                mCaptureResult = result;
+                return this;
+            }
+
+            public synchronized ImageSaverBuilder setCharacteristics(final CameraCharacteristics characteristics) {
+                if (characteristics == null) throw new NullPointerException();
+                mCharacteristics = characteristics;
+                return this;
+            }
+
+            public synchronized ImageSaver buildIfComplete() {
+                if(!isComplete()) {
+                    return null;
+                }
+                return new ImageSaver(mImage, mFile, mCaptureResult, mCharacteristics, mReader);
+            }
+
+            public synchronized String getSaveLocation() {
+                return (mFile == null) ? "Unknown" : mFile.toString();
+            }
+
+            public boolean isComplete() {
+                return mImage != null && mFile != null && mCaptureResult != null
+                        && mCharacteristics != null;
+            }
+
+        }
+
+    }
+
+private static String generateTimeStamp() {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyy_MM_dd_HH_mm_ss_SSS", Locale.US);
+    return sdf.format(new Date());
+}
+
+
 }
