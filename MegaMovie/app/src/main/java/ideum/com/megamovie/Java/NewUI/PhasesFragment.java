084335e78e9440977b1a1c9c2d0968e94251a0ef
nmbrax <braxton@ideum.com>
2019-01-14 13:18:40 -0700

    removed commented out code

--- MegaMovie/app/src/main/java/ideum/com/megamovie/Java/NewUI/PhasesFragment.java
+++ MegaMovie/app/src/main/java/ideum/com/megamovie/Java/NewUI/PhasesFragment.java
@@ -160,57 +160,25 @@ public class PhasesFragment extends Fragment {
             case CONTACT1:
                 caption = getResources().getString(R.string.first_contact_dialog_message);
                 title = "First Contact";
-//                if (!c1Expanded) {
-//                    learnMorec1.setText(caption);
-//                } else {
-//                    learnMorec1.setText("LEARN MORE");
-//                }
-//                c1Expanded = !c1Expanded;
                 break;
-
             case CONTACT2:
                 caption = getResources().getString(R.string.second_contact_dialog_message);
                 title = "Second Contact";
-//                if (!c2Expanded) {
-//                    learnMorec2.setText(caption);
-//                } else {
-//                    learnMorec2.setText("LEARN MORE");
-//                }
-//                c2Expanded = !c2Expanded;
                 break;
             case MIDDLE:
                 caption = getResources().getString(R.string.mid_eclipse_dialog_message);
                 title = "Totality";
-//                if (!cmExpanded) {
-//                    learnMorecm.setText(caption);
-//                } else {
-//                    learnMorecm.setText("LEARN MORE");
-//                }
-//                cmExpanded = !cmExpanded;
                 break;
             case CONTACT3:
                 caption = getResources().getString(R.string.third_contact_dialog_message);
                 title = "Third Contact";
-//                if (!c3Expanded) {
-//                    learnMorec3.setText(caption);
-//                } else {
-//                    learnMorec3.setText("LEARN MORE");
-//                }
-//                c3Expanded = !c3Expanded;
                 break;
             case CONTACT4:
                 caption = getResources().getString(R.string.fourth_contact_dialog_message);
                 title = "Fourth Contact";
-//                if (!c4Expanded) {
-//                    learnMorec4.setText(caption);
-//                } else {
-//                    learnMorec4.setText("LEARN MORE");
-//                }
-//                c4Expanded = !c4Expanded;
+
                 break;
         }
-
-
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setMessage(caption)
